8aa2d6620f6cee29ef2ab7afeed5832797a788f1
nryas <nryas@me.com>
2016-12-02 15:58:10 +0900

    Modify initialization process

---
+++ Assets/Scripts/SPHSimpleParticleSystem.cs
@@ -0,0 +1,229 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace SPHFluid
+{
+    public class SPHSimpleParticleSystem : MonoBehaviour
+    {
+        public struct Particle
+        {
+            public Vector2 Position;
+            public Vector2 Velocity;
+        }
+
+        public struct ParticleForce
+        {
+            public Vector2 Acceleration;
+        }
+
+        public struct ParticleDensity
+        {
+            public float Density;
+        }
+
+        [SerializeField]
+        ComputeShader SPHComputeShader;
+
+        [SerializeField]
+        Texture2D particleTexture;
+
+        [SerializeField]
+        Shader particleRenderShader;
+
+        ComputeBuffer SPHParticlesRead;
+
+        ComputeBuffer SPHParticlesWrite;
+
+        ComputeBuffer SPHParticlesForce;
+
+        ComputeBuffer SPHParticlesDensity;
+
+        Material particleMaterial;
+
+        #region Parameters
+
+        [SerializeField]
+        float RestDensity = 1000.0f;        // 定常密度   [kg / m^3]
+
+        [SerializeField]
+        float PressureCoef = 200.0f;        // 圧力係数
+
+        [SerializeField]
+        float Mass = 0.0002f;               // 粒子の質量 [kg]
+
+        [SerializeField]
+        float EffectiveRadius = 0.012f;     // 有効半径   [m]
+
+        [SerializeField]
+        float TimeStep = 0.005f;            // 微小時間   [s]
+
+        [SerializeField]
+        float ViscosityCoef = 0.1f;         // 粘性係数   [Pa * s = 1 kg / (m * s)]
+
+        [SerializeField]
+        float ParticleRadius = 0.003f;      // レンダリング時の粒子の半径
+
+        [SerializeField]
+        float WallStiffness = 10000.0f;     // 境界条件の係数
+
+        [SerializeField]
+        float ParticleInitGap = 0.0045f;    // 初期化時の隙間
+
+        [SerializeField]
+        Vector2 Gravity = new Vector2(0, -9.8f);
+
+        [SerializeField]
+        Vector2 MinBoundary = new Vector2(0.0f, 0.0f);
+
+        [SerializeField]
+        Vector2 MaxBoundary = new Vector2(3.0f, 3.0f);
+
+        [SerializeField]
+        Vector2 EmitPosition = new Vector2(0.0f, 0.0f);
+
+        float[] WallNormals
+        {
+            get
+            {
+                return new[] {
+                1f,   0,  -MinBoundary.x,  0,
+                 0,  1f,  -MinBoundary.y,  0,
+               -1f,   0,   MaxBoundary.x,  0,
+                 0, -1f,   MaxBoundary.y,  0, };
+            }
+        }
+
+        [SerializeField]
+        int maxParticles = 32768;
+
+        #endregion
+
+        #region Coefficients of Kernel Function
+
+        float Poly6Kernel { get { return 315.0f / (64.0f * Mathf.PI * Mathf.Pow(EffectiveRadius, 9.0f)); } }
+        float SpikeyKernel { get { return -45.0f / (Mathf.PI * Mathf.Pow(EffectiveRadius, 6.0f)); } }
+        float LapKernel { get { return 45.0f / (Mathf.PI * Mathf.Pow(EffectiveRadius, 6.0f)); } }
+
+        #endregion
+
+        // Use this for initialization
+        void Start()
+        {
+            particleMaterial = new Material(particleRenderShader);
+            particleMaterial.hideFlags = HideFlags.HideAndDontSave;
+
+            InitializeComputeBuffers();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnDrawGizmos()
+        {
+            Gizmos.DrawWireCube((MaxBoundary + MinBoundary) / 2, MaxBoundary - MinBoundary);
+        }
+
+        void OnRenderObject()
+        {
+            SetConstants();
+            int kernelId;
+
+            // Density
+            kernelId = SPHComputeShader.FindKernel("Density");
+            SPHComputeShader.SetBuffer(kernelId, "_ParticlesRead", SPHParticlesRead);
+            SPHComputeShader.SetBuffer(kernelId, "_ParticlesDensityWrite", SPHParticlesDensity);
+            SPHComputeShader.Dispatch(kernelId, maxParticles / 32, 1, 1);
+
+            // Force
+            kernelId = SPHComputeShader.FindKernel("Force");
+            SPHComputeShader.SetBuffer(kernelId, "_ParticlesRead", SPHParticlesRead);
+            SPHComputeShader.SetBuffer(kernelId, "_ParticlesDensityRead", SPHParticlesDensity);
+            SPHComputeShader.SetBuffer(kernelId, "_ParticlesForceWrite", SPHParticlesForce);
+            SPHComputeShader.Dispatch(kernelId, maxParticles / 32, 1, 1);
+
+            // Integration
+            kernelId = SPHComputeShader.FindKernel("Integrate");
+            SPHComputeShader.SetBuffer(kernelId, "_ParticlesRead", SPHParticlesRead);
+            SPHComputeShader.SetBuffer(kernelId, "_ParticlesForceRead", SPHParticlesForce);
+            SPHComputeShader.SetBuffer(kernelId, "_ParticlesWrite", SPHParticlesWrite);
+            SPHComputeShader.Dispatch(kernelId, maxParticles / 32, 1, 1);
+
+            // Render
+            particleMaterial.SetPass(0);
+            particleMaterial.SetMatrix("_InvViewMatrix", Camera.main.worldToCameraMatrix.inverse);
+            particleMaterial.SetTexture("_ParticleTexture", particleTexture);
+            particleMaterial.SetFloat("_ParticleSize", ParticleRadius);
+            particleMaterial.SetBuffer("_ParticlesBuffer", SPHParticlesWrite);
+            particleMaterial.SetBuffer("_ParticlesDensity", SPHParticlesDensity);
+            Graphics.DrawProcedural(MeshTopology.Points, maxParticles);
+
+
+            Swap(ref SPHParticlesRead, ref SPHParticlesWrite);
+
+        }
+
+        static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T tmp = lhs;
+            lhs = rhs;
+            rhs = tmp;
+        }
+
+        void InitializeComputeBuffers()
+        {
+            Particle[] particles = new Particle[maxParticles];
+            for (int i = 0; i < maxParticles; i++)
+            {
+                float x = EmitPosition.x + ParticleInitGap + ParticleInitGap * (i % 128);
+                float y = EmitPosition.y + ParticleInitGap + ParticleInitGap * (i / 128);
+                particles[i].Position = new Vector2(x, y);
+            }
+
+            ParticleForce[] particlesForce = new ParticleForce[maxParticles];
+            ParticleDensity[] particlesDensity = new ParticleDensity[maxParticles];
+
+            SPHParticlesRead = new ComputeBuffer(maxParticles, Marshal.SizeOf(typeof(Particle)));
+            SPHParticlesWrite = new ComputeBuffer(maxParticles, Marshal.SizeOf(typeof(Particle)));
+            SPHParticlesForce = new ComputeBuffer(maxParticles, Marshal.SizeOf(typeof(ParticleForce)));
+            SPHParticlesDensity = new ComputeBuffer(maxParticles, Marshal.SizeOf(typeof(ParticleDensity)));
+
+            SPHParticlesRead.SetData(particles);
+            SPHParticlesWrite.SetData(particles);
+            SPHParticlesForce.SetData(particlesForce);
+            SPHParticlesDensity.SetData(particlesDensity);
+        }
+
+        void SetConstants()
+        {
+            SPHComputeShader.SetFloat("_RestDensity", RestDensity);
+            SPHComputeShader.SetFloat("_PressureCoef", PressureCoef);
+            SPHComputeShader.SetFloat("_Mass", Mass);
+            SPHComputeShader.SetFloat("_EffectiveRadius", EffectiveRadius);
+            SPHComputeShader.SetFloat("_TimeStep", TimeStep);
+            SPHComputeShader.SetFloat("_Viscosity", ViscosityCoef);
+            SPHComputeShader.SetFloat("_WallStiffness", WallStiffness);
+            SPHComputeShader.SetFloat("_ParticleGap", ParticleInitGap);
+            SPHComputeShader.SetVector("_Gravity", Gravity);
+            SPHComputeShader.SetVector("_MinBoundary", MinBoundary);
+            SPHComputeShader.SetVector("_MaxBoundary", MaxBoundary);
+            SPHComputeShader.SetInt("_MaxParticles", maxParticles);
+            SPHComputeShader.SetFloat("_Poly6Kernel", Poly6Kernel);
+            SPHComputeShader.SetFloat("_SpikeyKernel", SpikeyKernel);
+            SPHComputeShader.SetFloat("_LapKernel", LapKernel);
+            SPHComputeShader.SetFloats("_WallNormals", WallNormals);
+        }
+
+        void OnDisable()
+        {
+            SPHParticlesRead.Release();
+            SPHParticlesWrite.Release();
+            SPHParticlesForce.Release();
+            SPHParticlesDensity.Release();
+        }
+    }
+}
--- Assets/Shaders/SPHRenderer.shader
+++ Assets/Shaders/SPHRenderer.shader
@@ -8,18 +8,35 @@
 	CGINCLUDE
 	#include "UnityCG.cginc"
 
-	float3 HUEtoRGB(in float H)
+	float3 HSVtoRGB(float3 HSV)
 	{
-		float R = abs(H * 6 - 3) - 1;
-		float G = 2 - abs(H * 6 - 2);
-		float B = 2 - abs(H * 6 - 4);
-		return saturate(float3(R, G, B));
+		float3 RGB = 0;
+		float C = HSV.z * HSV.y;
+		float H = HSV.x * 6;
+		float X = C * (1 - abs(fmod(H, 2) - 1));
+		if (HSV.y != 0)
+		{
+			float I = floor(H);
+			if (I == 0) { RGB = float3(C, X, 0); }
+			else if (I == 1) { RGB = float3(X, C, 0); }
+			else if (I == 2) { RGB = float3(0, C, X); }
+			else if (I == 3) { RGB = float3(0, X, C); }
+			else if (I == 4) { RGB = float3(X, 0, C); }
+			else { RGB = float3(C, 0, X); }
+		}
+		float M = HSV.z - C;
+		return RGB + M;
 	}
 
 	struct Particle
 	{
 		float2 position;
 		float2 velocity;
+	};
+
+	struct ParticleDensity
+	{
+		float density;
 	};
 
 	struct v2g
@@ -36,6 +53,7 @@
 	};
 
 	StructuredBuffer<Particle> _ParticlesBuffer;
+	StructuredBuffer<ParticleDensity> _ParticlesDensity;
 
 	sampler2D _ParticleTexture;
 	float4x4 _InvViewMatrix;
@@ -63,7 +81,10 @@
 		v2g o;
 		o.position.xy = _ParticlesBuffer[id].position;
 		o.position.z = _ParticleSize;
-		o.color = float4(HUEtoRGB(smoothstep(0, 10, length(_ParticlesBuffer[id].velocity))), 1);
+		//o.color = float4(HUEtoRGB(smoothstep(0, 10, length(_ParticlesBuffer[id].velocity))), 0.6);
+		// 白 <-- 低  [ 彩度 ] 高 --> 青
+
+		o.color = float4(HSVtoRGB(float3(0.8, smoothstep(2000, 1000, length(_ParticlesDensity[id].density)), 1)), 1.0);
 		return o;
 	}
 
@@ -96,7 +117,7 @@
 
 	SubShader
 	{
-		Tags{ "Queue" = "Transparent" "RenderType" = "Transparent" "IgnoreProjector" = "True" }
+		Tags{ "RenderType" = "Transparent" "IgnoreProjector" = "True" "RenderType" = "Transparent" }
 			Zwrite Off
 			Blend One One
 			Cull Off
